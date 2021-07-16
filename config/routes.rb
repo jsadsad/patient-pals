462fba44b7b1cb2fedbe51f64b25f37a4d9795ac
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-15 22:22:14 -0700

    starting session api util. fixed error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails
     root to: 'static_pages#root'
     namespace :api, defaults: { format: :json } do
       resources :users, only: %i[index show update create]
+      resource :session, only: %i[create destroy]
     end
-    resource :session, only: %i[create destroy]
   end
