81c646ddc836fe547f8e11f2972816bd19515ae7
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-15 21:38:16 -0700

    updated users model with SPIRE

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,50 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, :session_token, presence: true
+  validates :password,
+            length: {
+              minimum: 6,
+              message: 'Password is too short. Minimum is 6 characters.',
+            },
+            allow_nil: true
+  validates :role, inclusion: {
+          in: %i[patient patient_partner]
+          message: 'Please select your role',
+  }
+  validates :age,
+            inclusion: {
+              in: (13..149).to_a,
+              message: 'Must be 13 or older to sign up.',
+            }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
