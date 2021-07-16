e52e1746ba37808edef560172d23a4ce49ce35b9
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-15 23:35:38 -0700

    added route util along with updating App.jsx

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import LandingPage from './landing/landing'
+import LoginFormContainer from './session_form/login_container'
+import SignupFormContainer from './session_form/signup_container'
+import { AuthRoute, ProtectedRoute } from '../utils/route_util'
 
 const App = () => (
   <div>
-    <LandingPage />
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <Redirect to="/404" />
+    </Switch>
   </div>
 )
 
