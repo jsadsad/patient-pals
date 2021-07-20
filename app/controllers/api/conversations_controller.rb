e9cfb9c579b79466b386d196e821fe39d59345cb
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-19 20:21:38 -0700

    updated conversation controller

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -1,2 +1,22 @@
 class Api::ConversationsController < ApplicationController
+  before_action :require_logged_in
+
+  def index
+    @users = User.all
+    @conversations = Conversation.all
+  end
+
+  def create
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    else
+      @conversation = Conversation.create!(conversation_params)
+    end
+    render :show
+  end
+
+  private
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
 end
