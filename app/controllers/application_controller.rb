08afa28ba1c8b5df5e84803c6735b5fbadbf65c3
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-20 15:47:50 -0700

    fixed redirect error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_logged_in
-    redirect_to new_session_url unless logged_in?
+    redirect_to api_session_url unless logged_in?
   end
 
   def login(user)
