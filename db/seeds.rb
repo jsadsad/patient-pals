69781c1521207449785f5f30da6f437f03a50dcc
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-19 23:48:56 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,45 @@ User.create!(
   role: "Patient"
 )
 
-user1 =
+user2 =
 User.create!(
   first_name: 'Hank',
   last_name: 'Pym',
   email: 'test@example2.com',
   age: 50,
+  location: 'San Francisco',
+  password: 'password',
+  role: "Patient Partner"
+)
+
+user3 =
+User.create!(
+  first_name: 'Bruce',
+  last_name: 'Banner',
+  email: 'test@example3.com',
+  age: 35,
+  location: 'San Francisco',
+  password: 'password',
+  role: "Patient Partner"
+)
+
+user4 =
+User.create!(
+  first_name: 'Tony',
+  last_name: 'Stark',
+  email: 'test@example4.com',
+  age: 21,
+  location: 'San Francisco',
+  password: 'password',
+  role: "Patient"
+)
+
+user5 =
+User.create!(
+  first_name: 'Stephen',
+  last_name: 'Strange',
+  email: 'test@example5.com',
+  age: 29,
   location: 'San Francisco',
   password: 'password',
   role: "Patient Partner"
