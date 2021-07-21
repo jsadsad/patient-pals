75d1f8452b9efd996235897eccb615ec87c96f28
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-21 12:45:13 -0700

    working on navbar headers

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,58 +1,90 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
 
 const NavBar = ({ currentUser, logout }) => {
-  return (
-    <nav
-      class="navbar navbar-expand-lg navbar-light"
-      style={{ backgroundColor: '#e3f2fd' }}
-    >
-      <a className="navbar-brand" href="#">
-        Patient Pals
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarText"
-        aria-controls="navbarText"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarText">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <span className="nav-link">
-              {currentUser.firstName} {currentUser.lastName}
-            </span>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
+  const display = currentUser ? (
+    <ul className="navbar-nav">
+      <li className="nav-item dropdown">
+        <a
+          className="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdown"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Hello, {currentUser.firstName} {currentUser.lastName}!
+        </a>
+        <ul
+          className="dropdown-menu dropdown-menu-end"
+          aria-labelledby="navbarDropdown"
+        >
+          <li>
+            <a className="dropdown-item" href="#">
               Messsages
             </a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              All Users
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Patients
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Patient Partners
-            </a>
+          <li>
+            <div className="dropdown-item" onClick={logout}>
+              Log Out
+            </div>
           </li>
         </ul>
+      </li>
+    </ul>
+  ) : (
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Log In
+        </a>
+      </li>
+    </ul>
+  )
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light"
+      style={{ backgroundColor: '#e3f2fd' }}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Patient Pals
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Users
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Patients
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Patient Partners
+              </a>
+            </li>
+          </ul>
+          {display}
+        </div>
       </div>
     </nav>
   )
 }
 
-export default NavBar
+export default withRouter(NavBar)
