81b7cb98e46037b5c1c0621674c0d1d3d4b66631
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-21 22:17:31 -0700

    updated conversation table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_212442) do
+ActiveRecord::Schema.define(version: 2021_07_22_051523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_07_19_212442) do
     t.integer "recipient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sender_name"
+    t.string "recipent_name"
   end
 
   create_table "messages", force: :cascade do |t|
