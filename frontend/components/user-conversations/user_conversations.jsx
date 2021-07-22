ac9de17c68edeb637bc82f670ddac3b25479ffdb
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-21 17:21:14 -0700

    created component and container for user-conversations

---
+++ frontend/components/user-conversations/user_conversations.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class UserConversations extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div>Conversations go here</div>
+  }
+}
---
+++ frontend/components/user-conversations/user_conversations_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux'
+
+import UserConversations from './user_conversations'
+
+const mapStateToProps = ({ entities }) => {
+  return {}
+}
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserConversations)
