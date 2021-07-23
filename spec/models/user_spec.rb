c265ce59722ce8816f05c28e2558505864a36f4d
Joshua C. Sadsad <joshsad2@gmail.com>
2021-07-23 16:15:56 -0700

    ran prettier format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,23 +3,45 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validation tests' do
     it 'ensures first name presence' do
-      user = User.new(last_name: 'Fury', email: 'sample@email.com', role: 'Patient').save
+      user =
+        User.new(last_name: 'Fury', email: 'sample@email.com', role: 'Patient')
+          .save
       expect(user).to eq(false)
     end
     it 'ensures last name presence' do
-      user = User.new(first_name: 'Nick', email: 'sample2@email.com', role: 'Patient').save
+      user =
+        User.new(
+          first_name: 'Nick',
+          email: 'sample2@email.com',
+          role: 'Patient',
+        ).save
       expect(user).to eq(false)
     end
     it 'ensures email presence' do
-      user = User.new(first_name: 'Nick', last_name: 'Fury', role: 'Patient').save
+      user =
+        User.new(first_name: 'Nick', last_name: 'Fury', role: 'Patient').save
       expect(user).to eq(false)
     end
     it 'ensures role presence' do
-      user = User.new(first_name: 'Nick', last_name: 'Fury', email: 'test@example.com').save
+      user =
+        User.new(
+          first_name: 'Nick',
+          last_name: 'Fury',
+          email: 'test@example.com',
+        ).save
       expect(user).to eq(false)
     end
     it 'should save successfully' do
-      user = User.new(first_name: 'Nick', last_name: 'Fury', password: "password", email: 'test@example.com', age: 30, location: 'SHIELD', role: 'Patient Partner').save
+      user =
+        User.new(
+          first_name: 'Nick',
+          last_name: 'Fury',
+          password: 'password',
+          email: 'test@example.com',
+          age: 30,
+          location: 'SHIELD',
+          role: 'Patient Partner',
+        ).save
       expect(user).to eq(true)
     end
   end
